ad8ef34aa3b6f58e524f29760e298bb224c0df80
Sanjay Nadhavajhala <sanjay.nadhavajhala@gmail.com>
2021-06-16 15:27:21 -0700

    replace NATS wrapper submodule with opni-nats python package (#1)
    * replace NATS wrapper submodule with opni-nats python package
    
    * adding pyproject.toml
    
    * Refactoring train.py to follow correct async structure

--- README.md
+++ README.md
@@ -7,7 +7,21 @@
     The training controller service should also be running beforehand as well as should NATS, Traefik and Elasticsearch.
     ```
 * The job will train a Nulog model based on logs retrieved from Elasticsearch.
-*To test this service, you can send payload to the NATS subject "train" in this format
+*To test this service, you can send the payload below to the NATS subject "train". The training controller must be running as it will pick up and start the training job.
   ```
   payload = {"model_to_train": "nulog","time_intervals": [{"start_ts": TIMESTAMP_NANO, "end_ts": TIMESTAMP_NANO}, {"start_ts": TIMESTAMP_NANO, "end_ts": TIMESTAMP_NANO}, ...]}
   ```
+
+## Contributing
+We use `pre-commit` for formatting auto-linting and checking import. Please refer to [installation](https://pre-commit.com/#installation) to install the pre-commit or run `pip install pre-commit`. Then you can activate it for this repo. Once it's activated, it will lint and format the code when you make a git commit. It makes changes in place. If the code is modified during the reformatting, it needs to be staged manually.
+
+```
+# Install
+pip install pre-commit
+
+# Install the git commit hook to invoke automatically every time you do "git commit"
+pre-commit install
+
+# (Optional)Manually run against all files
+pre-commit run --all-files
+```
--- nulog-train/train.py
+++ nulog-train/train.py
@@ -9,31 +9,28 @@ import shutil
 import boto3
 import botocore
 from botocore.client import Config
-from nats_wrapper import NatsWrapper
+from botocore.exceptions import EndpointConnectionError
 from NuLogParser import LogParser
+from opni_nats import NatsWrapper
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(message)s")
 
 MINIO_SERVER_URL = os.environ["MINIO_SERVER_URL"]
 MINIO_ACCESS_KEY = os.environ["MINIO_ACCESS_KEY"]
 MINIO_SECRET_KEY = os.environ["MINIO_SECRET_KEY"]
-NATS_SERVER_URL = os.environ["NATS_SERVER_URL"]
-
-nw = NatsWrapper()
 
 
-def train_nulog_model(minio_client, windows_folder_path):
+async def train_nulog_model(minio_client, windows_folder_path):
     nr_epochs = 2
     num_samples = 0
     parser = LogParser()
     texts = parser.load_data(windows_folder_path)
-    logging.info("NuLog model being trained from scratch")
     tokenized = parser.tokenize_data(texts, isTrain=True)
     parser.tokenizer.save_vocab()
-    logging.info("vocab has been saved!")
     parser.train(tokenized, nr_epochs=nr_epochs, num_samples=num_samples)
     all_files = os.listdir("output/")
     if "nulog_model_latest.pt" in all_files and "vocab.txt" in all_files:
+        logging.info("Completed training model")
         minio_client.meta.client.upload_file(
             "output/nulog_model_latest.pt", "nulog-models", "nulog_model_latest.pt"
         )
@@ -43,9 +40,40 @@ def train_nulog_model(minio_client, windows_folder_path):
         logging.info("Nulog model and vocab have been uploaded to Minio.")
     else:
         logging.info("Nulog model was not able to be trained and saved successfully.")
+        return False
+    return True
 
 
-async def send_signal_to_inference(loop):
+async def minio_setup_and_download_data(minio_client):
+    try:
+        minio_client.meta.client.download_file(
+            "training-logs", "windows.tar.gz", "windows.tar.gz"
+        )
+        logging.info("Downloaded logs from minio successfully")
+
+        shutil.unpack_archive("windows.tar.gz", format="gztar")
+    except EndpointConnectionError:
+        logging.error(
+            f"Could not connect to minio with endpoint_url={MINIO_SERVER_URL} aws_access_key_id={MINIO_ACCESS_KEY} aws_secret_access_key={MINIO_SECRET_KEY} "
+        )
+        logging.info("Job failed")
+        return False
+
+    try:
+        minio_client.meta.client.head_bucket(Bucket="nulog-models")
+        logging.info("nulog-models bucket exists")
+    except botocore.exceptions.ClientError as e:
+        # If a client error is thrown, then check that it was a 404 error.
+        # If it was a 404 error, then the bucket does not exist.
+        error_code = e.response["Error"]["Code"]
+        if error_code == "404":
+            logging.info("nulog-models bucket does not exist so creating it now")
+            minio_client.create_bucket(Bucket="nulog-models")
+    return True
+
+
+async def send_signal_to_nats():
+    nw = NatsWrapper()
     nulog_payload = {
         "bucket": "nulog-models",
         "bucket_files": {
@@ -55,7 +83,6 @@ async def send_signal_to_inference(loop):
     }
     encoded_nulog_json = json.dumps(nulog_payload).encode()
     await nw.connect()
-    nw.add_signal_handler(loop)
     await nw.publish(nats_subject="model_ready", payload_df=encoded_nulog_json)
     logging.info(
         "Published to model_ready Nats subject that new Nulog model is ready to be used for inferencing."
@@ -63,46 +90,33 @@ async def send_signal_to_inference(loop):
     await nw.nc.close()
 
 
+def main():
+    loop = asyncio.get_event_loop()
+
+    minio_client = boto3.resource(
+        "s3",
+        endpoint_url=MINIO_SERVER_URL,
+        aws_access_key_id=MINIO_ACCESS_KEY,
+        aws_secret_access_key=MINIO_SECRET_KEY,
+        config=Config(signature_version="s3v4"),
+    )
+
+    setup_task = loop.create_task(minio_setup_and_download_data(minio_client))
+    if not loop.run_until_complete(setup_task):
+        return
+
+    train_task = loop.create_task(train_nulog_model(minio_client, "windows/"))
+    if not loop.run_until_complete(train_task):
+        return
+
+    nats_signal_task = loop.create_task(send_signal_to_nats())
+    loop.run_until_complete(nats_signal_task)
+
+    loop.close()
+
+
 if __name__ == "__main__":
     try:
-        minio_client = boto3.resource(
-            "s3",
-            endpoint_url=MINIO_SERVER_URL,
-            aws_access_key_id=MINIO_ACCESS_KEY,
-            aws_secret_access_key=MINIO_SECRET_KEY,
-            config=Config(signature_version="s3v4"),
-        )
-
-        logging.info("Connected to Minio client")
-        minio_client.meta.client.download_file(
-            "training-logs", "windows.tar.gz", "windows.tar.gz"
-        )
-        shutil.unpack_archive("windows.tar.gz", format="gztar")
-
-        windows_folder_path = "windows/"
-        bucket = minio_client.Bucket("nulog-models")
-        exists = True
-        try:
-            minio_client.meta.client.head_bucket(Bucket="nulog-models")
-        except botocore.exceptions.ClientError as e:
-            # If a client error is thrown, then check that it was a 404 error.
-            # If it was a 404 error, then the bucket does not exist.
-            error_code = e.response["Error"]["Code"]
-            if error_code == "404":
-                exists = False
-        if exists:
-            logging.info("nulog-models bucket exists")
-        else:
-            logging.info("nulog-models bucket does not exist so creating it now")
-            minio_client.create_bucket(Bucket="nulog-models")
-        logging.info("About to train model")
-        train_nulog_model(minio_client, windows_folder_path)
-        logging.info("Model completed training")
-        loop = asyncio.get_event_loop()
-        send_signal_inference_coroutine = send_signal_to_inference(loop)
-        loop.run_until_complete(send_signal_inference_coroutine)
-        loop.close()
+        main()
     except Exception as e:
-        logging.info(
-            "Cannot train Nulog model right now. Received exception {}".format(e)
-        )
+        logging.info(f"Nulog training failed. Exception {e}")
