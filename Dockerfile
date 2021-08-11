34f8011431345d99364509ba376980236faa19d2
Dan Bason <colonel.dan@gmail.com>
2021-08-11 14:24:19 +1200

    Update variables and refactor docker

--- Dockerfile
+++ Dockerfile
@@ -1,30 +1,8 @@
-FROM python:3.8-slim as base
-
-FROM base as builder
-
-RUN apt-get update \
-    && apt-get install gcc -y \
-    && apt-get clean
-
-RUN python -m venv /opt/venv
-ENV PATH="/opt/venv/bin:$PATH"
-
-COPY ./nulog-train/requirements.txt /requirements.txt
-RUN pip install --no-cache-dir -r /requirements.txt
-
-FROM base
-
-COPY --from=builder /opt/venv /opt/venv
-ENV PATH="/opt/venv/bin:$PATH"
+FROM rancher/opni-python-base:3.8-torch
 
 COPY ./nulog-train/ /app/
 COPY ./models/nulog/ /app/
 RUN chmod a+rwx -R /app
 WORKDIR /app
-
-ENV PATH /usr/local/nvidia/bin:/usr/local/cuda/bin:${PATH}
-ENV LD_LIBRARY_PATH /usr/local/nvidia/lib:/usr/local/nvidia/lib64
-ENV NVIDIA_VISIBLE_DEVICES all
-ENV NVIDIA_DRIVER_CAPABILITIES compute,utility
 
 CMD ["python", "train.py"]
--- nulog-train/train.py
+++ nulog-train/train.py
@@ -15,12 +15,13 @@ from opni_nats import NatsWrapper
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(message)s")
 
-MINIO_SERVER_URL = os.environ["MINIO_SERVER_URL"]
-MINIO_ACCESS_KEY = os.environ["MINIO_ACCESS_KEY"]
-MINIO_SECRET_KEY = os.environ["MINIO_SECRET_KEY"]
+S3_ENDPOINT = os.environ["S3_ENDPOINT"]
+S3_ACCESS_KEY = os.environ["S3_ACCESS_KEY"]
+S3_SECRET_KEY = os.environ["S3_SECRET_KEY"]
+S3_BUCKET = os.getenv("S3_BUCKET", "opni_nulog_models")
 
 
-async def train_nulog_model(minio_client, windows_folder_path):
+async def train_nulog_model(s3_client, windows_folder_path):
     nr_epochs = 2
     num_samples = 0
     parser = LogParser()
@@ -31,10 +32,10 @@ async def train_nulog_model(minio_client, windows_folder_path):
     all_files = os.listdir("output/")
     if "nulog_model_latest.pt" in all_files and "vocab.txt" in all_files:
         logging.info("Completed training model")
-        minio_client.meta.client.upload_file(
+        s3_client.meta.client.upload_file(
             "output/nulog_model_latest.pt", "nulog-models", "nulog_model_latest.pt"
         )
-        minio_client.meta.client.upload_file(
+        s3_client.meta.client.upload_file(
             "output/vocab.txt", "nulog-models", "vocab.txt"
         )
         logging.info("Nulog model and vocab have been uploaded to Minio.")
@@ -44,38 +45,38 @@ async def train_nulog_model(minio_client, windows_folder_path):
     return True
 
 
-async def minio_setup_and_download_data(minio_client):
+async def minio_setup_and_download_data(s3_client):
     try:
-        minio_client.meta.client.download_file(
+        s3_client.meta.client.download_file(
             "training-logs", "windows.tar.gz", "windows.tar.gz"
         )
-        logging.info("Downloaded logs from minio successfully")
+        logging.info("Downloaded logs from s3 successfully")
 
         shutil.unpack_archive("windows.tar.gz", format="gztar")
     except EndpointConnectionError:
         logging.error(
-            f"Could not connect to minio with endpoint_url={MINIO_SERVER_URL} aws_access_key_id={MINIO_ACCESS_KEY} aws_secret_access_key={MINIO_SECRET_KEY} "
+            f"Could not connect to s3 with endpoint_url={S3_ENDPOINT}"
         )
         logging.info("Job failed")
         return False
 
     try:
-        minio_client.meta.client.head_bucket(Bucket="nulog-models")
+        s3_client.meta.client.head_bucket(Bucket=S3_BUCKET)
         logging.info("nulog-models bucket exists")
     except botocore.exceptions.ClientError as e:
         # If a client error is thrown, then check that it was a 404 error.
         # If it was a 404 error, then the bucket does not exist.
         error_code = e.response["Error"]["Code"]
         if error_code == "404":
-            logging.info("nulog-models bucket does not exist so creating it now")
-            minio_client.create_bucket(Bucket="nulog-models")
+            logging.info("{S3_BUCKET} bucket does not exist so creating it now")
+            s3_client.create_bucket(Bucket=S3_BUCKET)
     return True
 
 
 async def send_signal_to_nats():
     nw = NatsWrapper()
     nulog_payload = {
-        "bucket": "nulog-models",
+        "bucket": S3_BUCKET,
         "bucket_files": {
             "model_file": "nulog_model_latest.pt",
             "vocab_file": "vocab.txt",
@@ -93,19 +94,19 @@ async def send_signal_to_nats():
 def main():
     loop = asyncio.get_event_loop()
 
-    minio_client = boto3.resource(
+    s3_client = boto3.resource(
         "s3",
-        endpoint_url=MINIO_SERVER_URL,
-        aws_access_key_id=MINIO_ACCESS_KEY,
-        aws_secret_access_key=MINIO_SECRET_KEY,
+        endpoint_url=S3_ENDPOINT,
+        aws_access_key_id=S3_ACCESS_KEY,
+        aws_secret_access_key=S3_SECRET_KEY,
         config=Config(signature_version="s3v4"),
     )
 
-    setup_task = loop.create_task(minio_setup_and_download_data(minio_client))
+    setup_task = loop.create_task(minio_setup_and_download_data(s3_client))
     if not loop.run_until_complete(setup_task):
         return
 
-    train_task = loop.create_task(train_nulog_model(minio_client, "windows/"))
+    train_task = loop.create_task(train_nulog_model(s3_client, "windows/"))
     if not loop.run_until_complete(train_task):
         return
 
