c48b87f87b5333ca40bc7c5f9e5a25a4017582f2
Dan Bason <colonel.dan@gmail.com>
2021-08-12 08:44:14 +1200

    Fix bucket name

--- nulog-train/train.py
+++ nulog-train/train.py
@@ -18,7 +18,7 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(messa
 S3_ENDPOINT = os.environ["S3_ENDPOINT"]
 S3_ACCESS_KEY = os.environ["S3_ACCESS_KEY"]
 S3_SECRET_KEY = os.environ["S3_SECRET_KEY"]
-S3_BUCKET = os.getenv("S3_BUCKET", "opni_nulog_models")
+S3_BUCKET = os.getenv("S3_BUCKET", "opni-nulog-models")
 
 
 async def train_nulog_model(s3_client, windows_folder_path):
